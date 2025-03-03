8b70d071b561b22066bb43d38dcd7df6a9f70f3e
B00158031 Sozi Fatima <B00158031@mytudublin.ie>
2025-03-03 15:37:03 +0000

    Menu bar working

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('product/displaygrid', 'App\Http\Controllers\productController@displaygrid')->name('products.displaygrid');
-
+Route::get('product/additem/{id}', 'App\Http\Controllers\productController@additem')->name('products.additem');
 Route::resource('products', App\Http\Controllers\productController::class);
 
 
