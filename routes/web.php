a3c23fb8d08731d4c016cddc32a0987bb8b7155a
B00158031 Sozi Fatima <B00158031@mytudublin.ie>
2025-03-03 16:40:45 +0000

    Empty the shopping cart function works

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,4 @@ Route::resource('orderdetails', App\Http\Controllers\orderdetailController::clas
 
 Route::get('product/displaygrid', 'App\Http\Controllers\productController@displaygrid')->name('products.displaygrid');
 Route::get('product/additem/{id}', 'App\Http\Controllers\productController@additem')->name('products.additem');
+Route::get('product/emptycart', 'App\Http\Controllers\productController@emptycart')->name('products.emptycart');
