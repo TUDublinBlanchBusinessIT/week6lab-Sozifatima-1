c122404abd3db3e7c26a7766d44e7c1183c6520b
B00158031 Sozi Fatima <B00158031@mytudublin.ie>
2025-03-03 16:23:56 +0000

    shoppingcart items working

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('product/displaygrid', 'App\Http\Controllers\productController@displaygrid')->name('products.displaygrid');
-Route::get('product/additem/{id}', 'App\Http\Controllers\productController@additem')->name('products.additem');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
 Route::resource('products', App\Http\Controllers\productController::class);
 
 
@@ -22,3 +25,6 @@ Route::resource('scorders', App\Http\Controllers\scorderController::class);
 
 
 Route::resource('orderdetails', App\Http\Controllers\orderdetailController::class);
+
+Route::get('product/displaygrid', 'App\Http\Controllers\productController@displaygrid')->name('products.displaygrid');
+Route::get('product/additem/{id}', 'App\Http\Controllers\productController@additem')->name('products.additem');
